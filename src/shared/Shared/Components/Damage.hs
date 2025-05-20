6f196da59e218ce5a9c709ba59d2ed39dc0c4d33
fl215 <andrew.lua80@gmail.com>
2025-05-20 06:37:39 +0300

    me when Damage.hs

--- src/shared/Shared/Components/Damage.hs
+++ src/shared/Shared/Components/Damage.hs
@@ -2,6 +2,8 @@
 module Shared.Components.Damage( Damage(..), DamageType(..), DamageModifier(..) ) where
 
 import Apecs
+import qualified Data.Map as M (Map)
+import Data.Map (unionWith, empty)
 
 -- | type of damage of the following:
 --
@@ -23,17 +25,25 @@ data DamageType =
   | Integral -- Structural
   | Bloodloss | Asphyxiation -- Airloss
   | Nerve | Stamina | Genetic | Bleeding -- General
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 -- | usually used for damage resistances, denotes a modification to damage
 data DamageModifier =
     DamageModifierPrecentage -- ^ modify the damage by a precentage
       DamageType -- ^ type of damage to modify
-      Int -- ^ precentage from 0 - 100
+      Double -- ^ precentage from 0 - 100
   | DamageModifierNum -- ^ modify the damage by a number
       DamageType -- ^ type of damage to modify
       Double -- ^ a number
   deriving (Eq, Show)
 
-newtype Damage = Damage [(DamageType, Double, Entity)] deriving Show
+-- | list of all damage of an entity
+newtype Damage = Damage { getDamage :: (M.Map DamageType Double) } deriving Show
+
+instance Semigroup Damage where
+  (<>) (Damage dmg1) (Damage dmg2) = Damage $ unionWith (+) dmg1 dmg2
+
+instance Monoid Damage where
+  mempty = Damage empty
+
 instance Component Damage where type Storage Damage = Map Damage
